89947bcd8174a34ef01f3a0442892b175ca40324
k790911 <k790911@gmail.com>
2022-01-20 22:37:01 +0900

    다크모드 작업

--- KxMemo/MemoListTableViewController.swift
+++ KxMemo/MemoListTableViewController.swift
@@ -75,6 +75,16 @@ class MemoListTableViewController: UITableViewController {
         //cell.detailTextLabel?.text = target.insertDate.description
         cell.detailTextLabel?.text = formatter.string(for: target.insertDate)
         
+        //낮은 버젼 다크모드 지원을 위한 코드로 색깔 바꾸는 코드, 커스텀으로 만든 컬러셋을 전달
+        // ios11에서 추가된 컬러셋은 낮은 os에서는 오류남
+        if #available(iOS 11.0, *) {
+            cell.detailTextLabel?.textColor = UIColor(named: "MyLabelColor")
+        } else {
+            // 낮은 버전에서 원하는 컬러를 쓰기 위해서는 RGB를 통해 설정하는 것이 맞음. 하지만 그냥 라잇그레이로 설정
+            cell.detailTextLabel?.textColor = UIColor.lightGray
+        }
+        
+        
         return cell
     }
     
